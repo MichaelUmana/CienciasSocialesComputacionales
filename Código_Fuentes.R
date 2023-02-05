1f3683b65d87ec2ded4a0309f999853ef6ac5c93
MichaelUmana <mi.umana.h@gmail.com>
2023-02-05 20:30:11 -0300

    Se hace una segundo código con el tratamiento de datos para poder separar el código fuente del tratamiento.

--- Código_Fuentes.R
+++ Código_Fuentes.R
@@ -7,5 +7,10 @@ install.packages("git2r")
 library(tidyverse)
 library(ggplot2)
 library(git2r)
+library(readr)
 
+#Se cargan los datos
 
+bbdd <- read.csv("./Data/M34_202103_eph.csv")
+head(bbdd)
+unique(bbdd)
---
+++ Código_Tratamiento.R
@@ -0,0 +1,40 @@
+#Tratamiento de datos 
+
+# Se hace el cambio de CHR a Factor por medio del siguiente codigo
+bbdd$CH04 <- as.factor(bbdd$CH04)
+
+# Se visualiza que la variable genero era CHR, por tanto, se deja como factor para poder hacer una visualización de datos posteriormente. 
+str(bbdd$CH04)
+
+#Se genera una visualización pre eliminar para poder esclarecer la situación 
+
+barplot(table(bbdd$CH04))
+
+#Se puede observar que la mayor cantindad de encuestados son varones posteriormente 
+
+plot.intro(bbdd$CH04)
+
+
+# Se hace una visualización de la base de datos para tener claridad de su composición.
+
+plot_intro(bbdd)
+
+#Se puede observar que no existe valores perdidos y posteriormente se hace una visualización de la variblae género de la base de datos-
+
+#Visualización variable genero dentro de la base de datos y posterior al paso de CHR a Factor.
+
+plot_bar(bbdd$CH04)
+
+#Siguiendo con la visualización de datos, se hace sobre la variable aglomerado.
+
+str(bbdd$AGLOMERADO)
+
+bbdd$AGLOMERADO <- as.factor(bbdd$AGLOMERADO)
+
+#Se hace una visualización de datos 
+
+plot_bar(bbdd$AGLOMERADO)
+
+
+#Para establecer las variables. 
+bbdd %>% plot_bar()
